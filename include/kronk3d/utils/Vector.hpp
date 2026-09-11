84c885845ecadb8e99ef5666598adb7759ef883f
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-09-11 21:26:53 +0200

    [UP] Update Vectors, now some conversion from Vector3 to Vector4.

--- include/kronk3d/utils/Vector.hpp
+++ include/kronk3d/utils/Vector.hpp
@@ -11,13 +11,12 @@ namespace k3
         inline Vector2(T x, T y) : x(x), y(y) {}
     };
 
-    template<typename T>
-    struct Vector3
-    {
-        T x, y, z;
+    // NOTE: Vector2's
+    using Vector2f = Vector2<float>;
+    using Vector2u = Vector2<unsigned int>;
+    using Vector2i = Vector2<int>;
+    using Vector2d = Vector2<double>;
 
-        inline Vector3(T x, T y, T z) : x(x), y(y), z(z) {}
-    };
 
     template<typename T>
     struct Vector4
@@ -27,21 +26,34 @@ namespace k3
         inline Vector4(T x, T y, T z, T a) : x(x), y(y), z(z), a(a) {}
     };
 
-    // NOTE: Vector2's
-    using Vector2f = Vector2<float>;
-    using Vector2u = Vector2<unsigned int>;
-    using Vector2i = Vector2<int>;
-    using Vector2d = Vector2<double>;
+    using Vector4f = Vector4<float>;
+    using Vector4u = Vector4<unsigned int>;
+    using Vector4i = Vector4<int>;
+    using Vector4d = Vector4<double>;
+
+
+    template<typename T>
+    struct Vector3
+    {
+        T x, y, z;
+
+        inline Vector3(T x, T y, T z) : x(x), y(y), z(z) {}
+
+        inline Vector4<T> asVector()
+        {
+            return Vector4<T>(x, y, z, 0.f);
+        }
+
+        inline Vector4<T> asPoint()
+        {
+            return Vector4<T>(x, y, z, 1.f);
+        }
+    };
 
     // NOTE: Vector3's
     using Vector3f = Vector3<float>;
     using Vector3u = Vector3<unsigned int>;
     using Vector3i = Vector3<int>;
     using Vector3d = Vector3<double>;
-
-    using Vector4f = Vector4<float>;
-    using Vector4u = Vector4<unsigned int>;
-    using Vector4i = Vector4<int>;
-    using Vector4d = Vector4<double>;
 
 }
