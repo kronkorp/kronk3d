bbd03dec627bce658474f66b841f65dfd06848fc
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-09-16 14:27:34 +0200

    [FIX] Fix vectors

--- modules/math/src/Vector.hpp
+++ modules/math/src/Vector.hpp
@@ -11,34 +11,24 @@ namespace k3::Math
 {
 
     template<typename T>
-    concept Numeric = std::is_integral<T>() || std::is_floating_point<T>();
+    concept Numeric = std::is_arithmetic_v<T>() && !std::is_same_v<T, bool>;
 
     template<Numeric T>
     struct Vector2
     {
-        T x, y;
-
-        inline Vector2(T x, T y) : x(x), y(y) {}
-        inline Vector2() : x(static_cast<T>(0)), y(static_cast<T>(0)) {}
+        T x{}, y{};
     };
 
     template<Numeric T>
     struct Vector3
     {
-        T x, y, z;
-
-        inline Vector3(T x, T y, T z) : x(x), y(y), z(z) {}
-        inline Vector3() : x(static_cast<T>(0)), y(static_cast<T>(0)), z(static_cast<T>(z)) {}
+        T x{}, y{}, z{};
     };
 
     template<Numeric T>
     struct Vector4
     {
-        T x, y, z, w;
-
-        inline Vector4(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) {}
-        inline Vector4() : x(static_cast<T>(0)), y(static_cast<T>(0)), z(static_cast<T>(0)), w(static_cast<T>(0)) {}
+        T x{}, y{}, z{}, w{};
     };
-
 
 }
