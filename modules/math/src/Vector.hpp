a0105cda5e98ec6461fd0129260c6a911d2022cf
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-09-16 16:16:24 +0200

    [ADD] Add Cmakelists to modules/math

--- modules/math/src/Vector.hpp
+++ modules/math/src/Vector.hpp
@@ -11,7 +11,7 @@ namespace k3::Math
 {
 
     template<typename T>
-    concept Numeric = std::is_arithmetic_v<T>() && !std::is_same_v<T, bool>;
+    concept Numeric = std::is_arithmetic_v<T> && !std::is_same_v<T, bool>;
 
     template<Numeric T>
     struct Vector2
@@ -30,5 +30,20 @@ namespace k3::Math
     {
         T x{}, y{}, z{}, w{};
     };
+
+    using Vector2f = Vector2<float>;
+    using Vector2i = Vector2<int>;
+    using Vector2d = Vector2<double>;
+    using Vector2u = Vector2<unsigned int>;
+
+    using Vector3f = Vector3<float>;
+    using Vector3i = Vector3<int>;
+    using Vector3d = Vector3<double>;
+    using Vector3u = Vector3<unsigned int>;
+
+    using Vector4f = Vector4<float>;
+    using Vector4i = Vector4<int>;
+    using Vector4d = Vector4<double>;
+    using Vector4u = Vector4<unsigned int>;
 
 }
