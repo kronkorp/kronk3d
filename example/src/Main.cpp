fcf7e93c0da23d4c961824fde79f2f3c74a0ac3b
GauthierMalfilatre <gauthier.malfilatre@epitech.eu>
2026-09-17 14:35:26 +0200

    [ADD] Add rectangle rendering (two triangles)

--- example/src/Main.cpp
+++ example/src/Main.cpp
@@ -43,13 +43,14 @@ int main(void)
 
     k3::Mesh mesh{
         .vertices = {
-            {0.f, 0.5f, 0.f},
             {-0.5f, -0.5f, 0.f},
+            {-0.5f, 0.5f, 0.f},
             {0.5f, -0.5f, 0.f},
+            {0.5f, 0.5f, 0.f},
         },
-        .colors = {k3::Math::Color::Red, k3::Math::Color::Blue, k3::Math::Color::Green},
-        .indices = {0, 1, 2},
-        .count = 3  // 3 segment
+        .colors = {k3::Math::Color::Red, k3::Math::Color::Blue, k3::Math::Color::Green, k3::Math::Color::Yellow},
+        .indices = {0, 1, 2, 2, 1, 3},
+        .count = 6  // 3 segment
     };
 
     k3::Viewport viewport{
